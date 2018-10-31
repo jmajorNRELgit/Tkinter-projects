3d71b6cf0c4cb9674a781ffdfe474b25530df8b3
jmajor <joshua.major@nrel.gov>
2018-10-31 13:43:13 -0600

    Animation and button programs

--- Window.py
+++ Window.py
@@ -12,14 +12,27 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
 
+        self.init_window()
+
 
     def init_window(self):
         self.master.title("GUI")
 
+        self.pack(fill = BOTH, expand = 1)
+
+        quitButton = Button(self, text = "Quit", command = self.client_exit)
+
+        quitButton.place(x=0, y=0)
+
+    def client_exit(self):
+        print('It worked')
 
 
-root = Tk()
+def run_window():
+    root = Tk()
 
-app = Window(root)
+    root.geometry("400x300")
 
-root.mainloop()
+    app = Window(root)
+
+    root.mainloop()
